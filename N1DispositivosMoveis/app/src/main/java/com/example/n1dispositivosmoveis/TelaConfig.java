214f919712bcb5a1b504e758e3bb3b0dfae243d0
ronaldo <ronaldo.jrk@gmail.com>
2021-09-22 18:59:13 -0300

    inicial

---
+++ N1DispositivosMoveis/app/src/main/java/com/example/n1dispositivosmoveis/TelaConfig.java
@@ -0,0 +1,14 @@
+package com.example.n1dispositivosmoveis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaConfig extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_config);
+    }
+}
