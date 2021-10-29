11d5012f2e80e364cdf657c1f434b994ef288ad6
ronaldo <ronaldo.jrk@gmail.com>
2021-10-29 01:18:43 -0300

    add status e marker

--- N1DispositivosMoveis/app/src/main/java/com/example/n1dispositivosmoveis/MapsActivity.java
+++ N1DispositivosMoveis/app/src/main/java/com/example/n1dispositivosmoveis/MapsActivity.java
@@ -3,14 +3,24 @@ package com.example.n1dispositivosmoveis;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,7 +29,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.n1dispositivosmoveis.databinding.ActivityMapsBinding;
 
@@ -28,6 +40,8 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+
+import java.text.DecimalFormat;
 
 public class MapsActivity extends FragmentActivity implements  OnMapReadyCallback , GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener{
 
@@ -48,7 +62,6 @@ public class MapsActivity extends FragmentActivity implements  OnMapReadyCallbac
         super.onCreate(savedInstanceState);
 
 
-
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -57,6 +70,8 @@ public class MapsActivity extends FragmentActivity implements  OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        TextView text = findViewById(R.id.Legenda);
+        text.setText("ronaldo esta aqui testando");
     }
 
     /**
@@ -86,12 +101,9 @@ public class MapsActivity extends FragmentActivity implements  OnMapReadyCallbac
         //myloca.getLatitude()
         LatLng brasil = new LatLng(myloca.getLatitude(), myloca.getLongitude());
         googleMap.addMarker(new MarkerOptions().position(brasil)
-
                 .position(brasil)
                 .title("Marker in Igreja do Bomfim"))
                 .setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-
-
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(brasil));*/
 
 
@@ -118,6 +130,7 @@ public class MapsActivity extends FragmentActivity implements  OnMapReadyCallbac
         mMap.setIndoorEnabled(true);
         mMap.setBuildingsEnabled(true);
 
+
         // habilita camada de tráfego
 
         mMap.setTrafficEnabled(trafegoSave);
@@ -130,13 +143,13 @@ public class MapsActivity extends FragmentActivity implements  OnMapReadyCallbac
         UiSettings mapUI = mMap.getUiSettings();
         // habilita: pan, zoom, tilt, rotate
         mapUI.setAllGesturesEnabled(true);
-        // habilita norte
-        //mapUI.setCompassEnabled(true);
-        // habilta contole do zoom
-        mapUI.setZoomControlsEnabled(true);
 
-        // habilita camada de localização (MyLocation)
-        //habilitaMyLocation();
+        // habilita norte
+        mapUI.setCompassEnabled(true);
+        // habilta contole do zoom
+        //mapUI.setZoomControlsEnabled(true);
+
+
         startLocationUpdates();
 
 
@@ -149,6 +162,7 @@ public class MapsActivity extends FragmentActivity implements  OnMapReadyCallbac
                 Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED) {
             // A permissão foi dada
+
             // Cria o cliente FusedLocation
             mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
             // Configura solicitações de localização
@@ -164,20 +178,55 @@ public class MapsActivity extends FragmentActivity implements  OnMapReadyCallbac
                     mMap.clear();
                     Location location=locationResult.getLastLocation();
                     LatLng brasil = new LatLng(location.getLatitude(), location.getLongitude());
-                    mMap.addMarker(new MarkerOptions().position(brasil)
 
+                   /* int height = 100;
+                    int width = 100;
+                    BitmapDrawable bitmapdraw=(BitmapDrawable)getResources().getDrawable(R.drawable.blankaar2);
+                    Bitmap b=bitmapdraw.getBitmap();
+                    Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);*/
+                    Marker marker = mMap.addMarker(new MarkerOptions()
                             .position(brasil)
-                            .title("atual"))
-                            .setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+                            .title("atual")
+                            .flat(true)
+                            );
+                    marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.carfinal));
+
+                    marker.setRotation(location.getBearing());
+                   // Marker marker = new MarkerOptions().position(brasil).flat(true).title("asd");
+
+                    /* mMap.addMarker(new MarkerOptions().position(brasil).flat(true)
+                            .title("atual")).setIcon(BitmapDescriptorFactory.fromResource(R.drawable.blankfigmaleft));
+*/
+                     //.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.blankaar2));
+                    //.setIcon();
+
+                    /*imageView.setImageBitmap(
+                            decodeSampledBitmapFromResource(getResources(),R.drawable.blankaar2, 100, 100));*/
 
 
-                   // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(brasil, 15.0f));
-                    //mMap.moveCamera(brasil);
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(brasil )      // Sets the center of the map to Mountain View
+                            .zoom(17)                   // Sets the zoom
+                            .bearing(90)                // Sets the orientation of the camera to east
+                            .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                            .build();                   // Creates a CameraPosition from the builder
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(brasil));
 
 
 
+                    TextView text = findViewById(R.id.Legenda);
+                    text.setText("Latitude ");
+
+
+
+                    String status="";
+
+                    status+=getLatLong(location)+getVelocidade(location);
+
+
+                    text.setText(status);
 
                 }
             };
@@ -189,6 +238,98 @@ public class MapsActivity extends FragmentActivity implements  OnMapReadyCallbac
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_LOCATION_UPDATES);
         }
+    }
+
+    private String getVelocidade(Location location){
+
+        SharedPreferences prefs = getSharedPreferences("projeto", MODE_PRIVATE);
+        String coordenadasSave = prefs.getString("coordenadas", "notDefined");
+        String velocidadeSave = prefs.getString("velocidade", "notDefined");
+
+        String [] velocidade = {"Km/h","Mph",};
+        Log.d("velocidade", velocidadeSave+location.getSpeed()*3.6);
+        if(velocidadeSave=="notDefined"){
+            return String.valueOf("Velocidade(km/h)="+location.getSpeed()*3.6);
+        }else{
+            if(velocidadeSave=="Km/h"){
+                return String.valueOf("Velocidade(Km/h)="+location.getSpeed()*3.6);
+            }else{
+                if(velocidadeSave=="Mph"){
+                    return String.valueOf("Velocidade(Mph)="+location.getSpeed());
+                }else{
+                    return "Velocidade()=";
+                }
+            }
+
+        }
+
+
+
+
+
+    }
+    private String getLatLong(Location location){
+
+        SharedPreferences prefs = getSharedPreferences("projeto", MODE_PRIVATE);
+        String coordenadasSave = prefs.getString("coordenadas", "notDefined");
+
+        Log.d("coordenadas", coordenadasSave);
+
+        String [] coordenadas = {"Grau decimal","Grau-Minuto decimal","Grau-Minuto-Segundo decimal"};
+        if(coordenadasSave=="notDefined"){
+            /*return String.valueOf("Latitude(graus)="+location.getLatitude())+"\n"
+                    +String.valueOf("Longitude(graus)="+location.getLongitude())+"\n";*/
+            return String.valueOf("Latitude(Grau)="+Location.convert(location.getLatitude(),Location.FORMAT_DEGREES))+"\n"
+                    +String.valueOf("Longitude(Grau)="+Location.convert(location.getLongitude(),Location.FORMAT_DEGREES))+"\n";
+        }else{
+            if(coordenadasSave=="Grau decimal"){
+                 return String.valueOf("Latitude(Graus)="+location.getLatitude())+"°"+"\n"
+                        +String.valueOf("Longitude(Graus)="+location.getLongitude())+"°"+"\n";
+               /* return String.valueOf("Latitude(Grau)="+Location.convert(location.getLatitude(),Location.FORMAT_DEGREES))+"\n"
+                        +String.valueOf("Longitude(Grau)="+Location.convert(location.getLongitude(),Location.FORMAT_DEGREES))+"\n";*/
+            }else{
+                if(coordenadasSave=="Grau-Minuto decimal"){
+                    DecimalFormat df = new DecimalFormat("#,###.00");
+                    df.format(1234.36);
+                    double numLat =location.getLatitude();
+                    double intLat = Math.floor(numLat);
+                    double decimalLat =numLat % 1;
+                    decimalLat= Math.abs(decimalLat*60);
+                    String latitude =intLat+"°"+decimalLat+"’";
+
+                    double numLong =location.getLongitude();
+                    double intLong = Math.floor(numLong);
+                    double decimalLong =numLong % 1;
+                    decimalLong= Math.abs(decimalLong*60);
+                    String longitude =intLong+"°"+decimalLong+"’";
+
+                    //+Location.convert(location.getLatitude(),Location.FORMAT_SECONDS))+"\n"
+
+                   /* return String.valueOf("Latitude(Grau-Minuto)="+latitude)+"\n"
+                            +String.valueOf("Longitude(Grau-Minuto)="+longitude)+"\n";*/
+
+                    return String.valueOf("Latitude(Grau-Minuto)="+Location.convert(location.getLatitude(),Location.FORMAT_MINUTES))+"\n"
+                            +String.valueOf("Longitude(Grau-Minuto)="+Location.convert(location.getLongitude(),Location.FORMAT_MINUTES))+"\n";
+
+                }else{
+                        if(coordenadasSave=="Grau-Minuto-Segundo decimal"){
+                           /* return String.valueOf("Latitude(graus)4="+location.getLatitude())+"\n"
+                                    +String.valueOf("Longitude(graus)="+location.getLongitude())+"\n";*/
+
+                            return String.valueOf("Latitude(Grau-Minuto-Segundo)="+Location.convert(location.getLatitude(),Location.FORMAT_SECONDS))+"\n"
+                                    +String.valueOf("Longitude(Grau-Minuto-Segundo)="+Location.convert(location.getLongitude(),Location.FORMAT_SECONDS))+"\n";
+                        }else{
+                            return "Latitude()="+""+"\n"
+                                    +"Longitude()="+""+"\n";
+                        }
+                }
+            }
+        }
+
+
+
+
+
     }
 
     private void habilitaMyLocation() {
